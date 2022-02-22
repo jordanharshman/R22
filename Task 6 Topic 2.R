d5f7a1f9de27cd6dbc4a47b1b8a4e60a76768da6
Jordan Harshman <jth0083@auburn.edu>
2022-02-22 15:17:02 -0600

    Feb 22

--- Task 6 Topic 2.R
+++ Task 6 Topic 2.R
@@ -11,7 +11,23 @@ copus %>%
   group_by(Size, NewLayout) %>%
   summarize(Avg = mean(Lec.c))
 
-# 2 Instructor ID #358  (biologist) has requested a data set that shows how much more or less they engage in all 25 behaviors from the average biology instructor in each of her 35 classes. Export a spreadsheet with just the requested 25 variables plus a dummy class identifier.
+# 2 Instructor ID #358  (biologist) has requested a data set that shows how much more or less they engage in all 25 behaviors from the average biology instructor in each of her 16 classes. Export a spreadsheet with just the requested 25 variables plus a dummy class identifier.
 
+AvgBio <- copus %>%
+  filter(Broader == "Biological") %>%
+  summarize(across(L:T_O, mean)) %>%
+  slice(rep(1:n(), each = 16))
+
+dat358 <- copus %>%
+  filter(`Instructor ID` == 358) %>%
+  select(L:T_O)
+
+soln <- (dat358 - AvgBio) %>%
+  mutate(Class = 1:n())
+
+write_csv(soln, "export.csv")
 
 # 3 General research question: To what extent do biology instructors employ different teaching behaviors in small, medium, and large classes?
+
+
+
