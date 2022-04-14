6fad4d4f60fe1d596fbe2507887d2c7b16513927
Jordan Harshman <jth0083@auburn.edu>
2022-04-14 15:01:40 -0500

    original

---
+++ Phycocyanin Class Script.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(readxl)
+
+phyco <- read_excel("Phycocyanin/phycocyanin.xlsx") %>%
+  select(Enclosure, Treatment, Interval, `Temp ©`, `Phycocyanin (ug/L)`, 
+         `Secchi (cm)`, `DO (mg/L)`) %>%
+  rename(Day = Interval, ID = Enclosure, PC = `Phycocyanin (ug/L)`,
+         Secci = `Secchi (cm)`, DO = `DO (mg/L)`, Temp = `Temp ©`) %>%
+  mutate(ID = as.numeric(str_remove(ID, "Community-")),
+         Day = as.numeric(recode(Day, "0hr." = "0", "2hr." = "0.0833", "24hr." = "1", "48hr." = "2",
+                                 "72hr." = "3", "1week" = "7", "2week" = "14")),
+         PC = as.numeric(PC)) %>%
+  filter(Treatment != "Pond",
+         Day != 0.0833)
